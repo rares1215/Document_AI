e9d588b50bb0ce5646690f4dd566e37ae36276cc
Rares Giurgescu <raresgiurgescu@gmail.com>
2025-11-23 19:29:44 +0200

    Added automation in signals.py deleting the file from and the cahced analysis when we are deleting a resume/analysis

--- backend/api/singals.py
+++ backend/api/singals.py
@@ -1,9 +1,11 @@
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save,post_delete
 
 from .models import Resume,ResumeAnalysis
 from .services.extract_pdf import extract_text_from_pdf
 from .services.llm_analyzer import analyze_resume_with_llm
+from django.core.cache import cache
+import os
 
 
 ### made a reciver signal to create a resume_analysis when a resume si uploaded
@@ -31,3 +33,32 @@ def generate_resume_analysis(sender,instance,created,**kwargs):
         suggestions=ai_data.get("suggestions", ""),
         raw_llm_res=ai_data,
     )
+
+
+
+
+##### Reiver to delete cached analysis from cache if we delete a resume/analysis ####
+@receiver(post_delete,sender = ResumeAnalysis)
+def delete_cache_analysis(sender,instance,**kwargs):
+    try:
+        ### we get the id of the current analysis using instance
+        cache_key = f"resume analysis:{instance.id}"
+        #### Deleteing the current analysis
+        cache.delete(cache_key)
+        print(f"Deleted analysis{instance.id} from cache!")
+
+    ### Error in case if something goes wrong
+    except Exception as e:
+        print(f"Could not delete analysis from cache. {e}")
+
+####### Reciver to delete the file from media when we delete it from the db
+@receiver(post_delete,sender=Resume)
+def delete_file_from_media(sender,instance,**kwargs):
+    if instance.doc_file and hasattr(instance.doc_file, 'path'):
+        try:
+            file_path = instance.doc_file.path
+            if os.path.isfile(file_path):
+                print(f"Removed file{file_path} from media!")
+                os.remove(file_path)
+        except Exception as e:
+            print(f"Could not delete the resume {e}")
